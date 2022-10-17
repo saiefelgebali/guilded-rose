e080895a1cb569d229e491ba1b889bd67b3c71e3
Seif Ahmed <seif.ahmed@softwire.com>
2022-10-17 13:50:19 +0100

    ADD: Comments to original

--- original.js
+++ original.js
@@ -77,6 +77,7 @@ class Shop {
 
 	updateQuality() {
 		for (let i = 0; i < this.items.length; i++) {
+			// Handle default
 			if (
 				this.items[i].name != "Aged Brie" &&
 				this.items[i].name !=
@@ -98,9 +99,13 @@ class Shop {
 					}
 				}
 			}
+
+			// Update sellIn
 			if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
 				this.items[i].sellIn = this.items[i].sellIn - 1;
 			}
+
+			// Handle expired
 			if (this.items[i].sellIn < 0) {
 				if (this.items[i].name != "Aged Brie") {
 					if (
